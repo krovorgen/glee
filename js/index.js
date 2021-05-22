dfc3d2ef56aa7739d12e9f9bde34ae93220428a8
Biarozka <maksim.biarozka@infourok.biz>
2021-05-22 18:42:52 +0300

    Updates

---
+++ js/index.js
@@ -0,0 +1,27 @@
+$(function () {
+    //slick slider
+    $('.hero__items').slick({
+        arrows: false,
+        dots: true,
+    });
+
+    $('.company__items').slick({
+        arrows: false,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        swipeToSlide: true,
+    });
+
+    //mixitup
+    let containerEl1 = document.querySelector('.design');
+    let containerEl2 = document.querySelector('.products');
+    let config = {
+        controls: {
+            scope: 'local',
+        },
+    };
+    (() => mixitup(containerEl1, config))();
+    (() => mixitup(containerEl2, config))();
+});
